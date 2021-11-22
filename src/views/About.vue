3a34c4ed4699184c2e19c2485ca03e00c9166053
Albert Attard <albert.attard@thoughtworks.com>
2021-11-22 21:28:39 +0100

    Update stypes

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,10 @@
     <h1>We are working hard to get this up and running</h1>
   </div>
 </template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+</style>
