f59c8899d363110ea49bba45969bb8a4b81d94de
Albert Attard <albert.attard@thoughtworks.com>
2021-11-23 13:30:01 +0100

    Configure prettier

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="about">
     <h2>Prime directive</h2>
-    <blockquote cite="https://www.thoughtworks.com/en-de/insights/blog/applying-prime-directive-beyond-retrospective">Regardless of what we discover, we understand and truly believe that everyone did the best job they could, given what they knew at the time, their skills and abilities, the resources available, and the situation at hand.</blockquote>
+    <blockquote cite="https://www.thoughtworks.com/en-de/insights/blog/applying-prime-directive-beyond-retrospective">
+      Regardless of what we discover, we understand and truly believe that everyone did the best job they could, given
+      what they knew at the time, their skills and abilities, the resources available, and the situation at hand.
+    </blockquote>
     <p>Norm Kerth<br />Project Retrospectives: A Handbook for Team Review</p>
     <h2>Sociable Weaver</h2>
-    <p>As an author of programming blogs or books it is hard to make sure that the examples provided in the text work well.  As a reader, it is frustrating to find halfway through that the example does not work or how some things ended up here or not there.</p>
+    <p>
+      As an author of programming blogs or books it is hard to make sure that the examples provided in the text work
+      well. As a reader, it is frustrating to find halfway through that the example does not work or how some things
+      ended up here or not there.
+    </p>
     <p>Sociable Weaver is a platform created to solve these problems.</p>
-    <p><strong>For the authors</strong> - blogs or books Sociable Weaver provides a testing platform where all examples can be executed to make sure that these work and produce the expected results.</p>
-    <p><strong>For the readers</strong> - Sociable Weaver provides an interactive platform where all examples can be executed through the interactive web interface and viewed locally using the preferred IDE.</p>
+    <p>
+      <strong>For the authors</strong> - blogs or books Sociable Weaver provides a testing platform where all examples
+      can be executed to make sure that these work and produce the expected results.
+    </p>
+    <p>
+      <strong>For the readers</strong> - Sociable Weaver provides an interactive platform where all examples can be
+      executed through the interactive web interface and viewed locally using the preferred IDE.
+    </p>
   </div>
 </template>
 
