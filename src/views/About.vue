780a849d54916581d436377585fa76fb41f80a39
Albert Attard <albert.attard@thoughtworks.com>
2021-11-23 13:10:35 +0100

    Update the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <h1>We are working hard to get this up and running</h1>
+    <h1>Sociable Weaver</h1>
+    <p>
+      As an author of programming blogs or books it is hard to make sure that the examples provided in the text work
+      well.  As a reader, it is frustrating to find halfway through that the example does not work or how some things
+      ended up here or not there.
+    </p>
+    <p>Sociable Weaver is a platform created to solve these problems.</p>
+    <p>
+      <strong>For the authors</strong> - blogs or books Sociable Weaver provides a testing platform where all examples
+      can be executed to make sure that these work and produce the expected results.
+    </p>
+    <p>
+      <strong>For the readers</strong> - Sociable Weaver provides an interactive platform where all examples can be
+      executed through the interactive web interface and viewed locally using the preferred IDE.
+    </p>
   </div>
 </template>
 
--- tests/e2e/specs/about.js
+++ tests/e2e/specs/about.js
@@ -1,6 +1,6 @@
 describe("About Page", () => {
   it("renders the site information", () => {
     cy.visit("/#/about");
-    cy.contains("h1", "We are working hard to get this up and running");
+    cy.contains("h1", "Sociable Weaver");
   });
 });
