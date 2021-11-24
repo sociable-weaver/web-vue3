d1dc4482af7bb57756be250183541cf2ab2cd33f
Albert Attard <albert.attard@thoughtworks.com>
2021-11-24 18:01:10 +0100

    Change the check endpoint
    Encountered an unexpected issue related the CORS and the Spring Actuator Endpoints.  This issues does not appear with the custom endpoint.  Will investigate this issue further.

--- src/services/AppApi.ts
+++ src/services/AppApi.ts
@@ -1,17 +1,21 @@
 import { apiClient } from "@/services/ServiceApi";
 
 export function isAppRunning(): Promise<AppStatus> {
-  return apiClient
-    .get("/actuator/health")
-    .then((response) => response.status)
-    .then((status) => (status == 200 ? AppStatus.Healthy : AppStatus.Unhealthy))
-    .catch((e) => {
-      if (e.message !== "Network Error") {
-        throw e;
-      }
+  return (
+    apiClient
+      // TODO: Having an issue with the Spring actuator endpoints and CORS
+      // .get("/actuator/health")
+      .get("/demo")
+      .then((response) => response.status)
+      .then((status) => (status == 200 ? AppStatus.Healthy : AppStatus.Unhealthy))
+      .catch((e) => {
+        if (e.message !== "Network Error") {
+          throw e;
+        }
 
-      return AppStatus.CannotBeReached;
-    });
+        return AppStatus.CannotBeReached;
+      })
+  );
 }
 
 export enum AppStatus {
