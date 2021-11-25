5928450864d07a37dac42cdbdc27b5525abbc03d
Albert Attard <albert.attard@thoughtworks.com>
2021-11-25 18:59:41 +0100

    Fix issue with linting

--- clean-test.sh
+++ clean-test.sh
@@ -4,7 +4,4 @@ rm -rf cypress
 rm -rf dist
 rm -rf node_modules
 
-# There is an issue with the file:line `tests/e2e/plugins/index.js:12`
-npm run lint
-
-npm install && npm run test:unit && npm run test:e2e
+npm install && npm run lint && npm run test:unit && npm run test:e2e
--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,22 +1,7 @@
-/* eslint-disable arrow-body-style */
-// https://docs.cypress.io/guides/guides/plugins-guide.html
-
-// if you need a custom webpack configuration you can uncomment the following import
-// and then use the `file:preprocessor` event
-// as explained in the cypress docs
-// https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
-
-// /* eslint-disable import/no-extraneous-dependencies, global-require */
-// const webpack = require('@cypress/webpack-preprocessor')
-
+/* eslint @typescript-eslint/no-var-requires: "off" */
 const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 
 module.exports = (on, config) => {
-  // on('file:preprocessor', webpack({
-  //  webpackOptions: require('@vue/cli-service/webpack.config'),
-  //  watchOptions: {}
-  // }))
-
   on("task", { downloadFile });
 
   return Object.assign({}, config, {
