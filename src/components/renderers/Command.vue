50bf4df29e5ddce4fc99310187e963a1093fe639
Albert Attard <albert.attard@thoughtworks.com>
2021-12-19 19:31:28 +0100

    Edit command (sociable-weaver/backlog#20)

--- src/components/renderers/Command.vue
+++ src/components/renderers/Command.vue
@@ -1,9 +1,56 @@
 <template>
-  <pre>{{ command }}</pre>
+  <div v-if="entry.edit === true">
+    <div role="working-directory">
+      <label>Working Directory</label>
+      <input type="text" v-model="edit.workingDirectory" role="working-directory" />
+    </div>
+    <div role="command">
+      <label>Command</label>
+      <textarea v-model="editCommand" placeholder="command" role="command" />
+    </div>
+    <div role="variables">
+      <div class="header">Variables</div>
+      <div v-for="(variable, index) in edit.variables" :key="variable" role="variable">
+        <input @change="onUpdateVariable(variable, index, $event)" :value="variable" role="update-variable" />
+        <button @click="onRemoveVariable(index)" role="remove-variable">Remove</button>
+      </div>
+      <div>
+        <input v-model="newVariable" placeholder="Variable Name" role="new-variable" />
+        <button @click="onAddVariable" role="add-variable">Add</button>
+      </div>
+    </div>
+    <div role="missing-variables">
+      <div class="header">Potential missing variables</div>
+      <div v-for="variable in missingVariables" :key="variable" role="missing-variable">
+        <span>{{ variable }}</span>
+        <button @click="onAddMissingVariable(variable)">Add</button>
+      </div>
+    </div>
+    <div role="environment-variables">
+      <div class="header">Environment Variables</div>
+      <div v-for="(variable, index) in edit.environmentVariables" :key="variable" role="environment-variable">
+        <input
+          @change="onUpdateEnvironmentVariable(variable, index, $event)"
+          :value="variable"
+          role="update-environment-variable"
+        />
+        <button @click="onRemoveEnvironmentVariable(index)" role="remove-environment-variable">Remove</button>
+      </div>
+      <div>
+        <input
+          v-model="newEnvironmentVariable"
+          placeholder="Environment Variable Name"
+          role="new-environment-variable"
+        />
+        <button @click="onAddEnvironmentVariable" role="add-environment-variable">Add</button>
+      </div>
+    </div>
+  </div>
+  <pre v-else>{{ command }}</pre>
 </template>
 
 <script lang="ts">
-import { Entry, interpolate } from "@/models/Chapter";
+import { createSaveEntry, Entry, interpolate, OnSaveOutcome, OnSaveResult, SaveEntry } from "@/models/Chapter";
 import { Options, Vue } from "vue-class-component";
 
 @Options({
@@ -14,6 +61,14 @@ import { Options, Vue } from "vue-class-component";
 })
 export default class Command extends Vue {
   private entry!: Entry;
+  private edit: SaveEntry = { parameters: [""] } as SaveEntry;
+  private newVariable = "";
+  private newEnvironmentVariable = "";
+
+  mounted(): void {
+    this.entry.onSave = this.onSave;
+    this.edit = createSaveEntry(this.entry);
+  }
 
   get command(): string {
     const workingDirectory = this.entry.workingDirectory ? `${this.entry.workingDirectory} ` : "";
@@ -21,6 +76,154 @@ export default class Command extends Vue {
     let command = `${workingDirectory}${commandPromptSymbol} ${this.entry.parameters.join("\n")}`;
 
     return interpolate(this.entry.variables, this.entry.values, command);
+  }
+
+  get editCommand(): string {
+    return this.edit.parameters.join("\n");
+  }
+
+  set editCommand(value: string) {
+    this.edit.parameters = value.split("\n");
+  }
+
+  get missingVariables(): string[] {
+    const command = this.edit.parameters.join("\n");
+    const match = command.match(Command.variableNameRegex()) || [];
+    return match.map((v) => v.substring(2, v.length - 1)).filter((v) => !this.edit.variables.includes(v));
+  }
+
+  private onAddVariable(): void {
+    this.entry.error = "";
+    const variable = this.newVariable.trim().toUpperCase();
+
+    if (variable.length === 0) {
+      this.entry.error = "Cannot add a variable without name";
+      return;
+    }
+
+    const regex = Command.variableNameRegex();
+    if (!regex.test(`\${${variable}}`)) {
+      this.entry.error = "Invalid variable name.  Only letters, numbers, underscore and dash are permitted";
+      return;
+    }
+
+    if (this.edit.variables.includes(variable)) {
+      this.entry.error = "Another variable with the same name already exists";
+      return;
+    }
+
+    this.edit.variables.push(variable);
+    this.newVariable = "";
+  }
+
+  private onUpdateVariable(currentValue: string, index: number, event: Event): void {
+    this.entry.error = "";
+
+    const target = event.target as HTMLInputElement;
+    const variable = target.value.trim().toUpperCase();
+
+    if (currentValue === variable) {
+      target.value = currentValue;
+      return;
+    }
+
+    if (variable.length === 0) {
+      this.entry.error = "Cannot have a variable without name";
+      target.value = currentValue;
+      return;
+    }
+
+    if (this.edit.variables.includes(variable)) {
+      this.entry.error = "Another variable with the same name already exists";
+      target.value = currentValue;
+      return;
+    }
+
+    this.edit.variables[index] = variable;
+  }
+
+  private onRemoveVariable(index: number): void {
+    this.edit.variables.splice(index, 1);
+  }
+
+  private onAddEnvironmentVariable(): void {
+    this.entry.error = "";
+    const variable = this.newEnvironmentVariable.trim().toUpperCase();
+
+    if (variable.length === 0) {
+      this.entry.error = "Cannot add an environment variable without name";
+      return;
+    }
+
+    const regex = Command.variableNameRegex();
+    if (!regex.test(`\${${variable}}`)) {
+      this.entry.error = "Invalid environment variable name.  Only letters, numbers, underscore and dash are permitted";
+      return;
+    }
+
+    if (this.edit.environmentVariables.includes(variable)) {
+      this.entry.error = "Another environment variable with the same name already exists";
+      return;
+    }
+
+    this.edit.environmentVariables.push(variable);
+    this.newEnvironmentVariable = "";
+  }
+
+  private onUpdateEnvironmentVariable(currentValue: string, index: number, event: Event): void {
+    this.entry.error = "";
+
+    const target = event.target as HTMLInputElement;
+    const variable = target.value.trim().toUpperCase();
+
+    if (currentValue === variable) {
+      target.value = currentValue;
+      return;
+    }
+
+    if (variable.length === 0) {
+      this.entry.error = "Cannot have a variable without name";
+      target.value = currentValue;
+      return;
+    }
+
+    if (this.edit.environmentVariables.includes(variable)) {
+      this.entry.error = "Another variable with the same name already exists";
+      target.value = currentValue;
+      return;
+    }
+
+    this.edit.environmentVariables[index] = variable;
+  }
+
+  private onRemoveEnvironmentVariable(index: number): void {
+    this.edit.environmentVariables.splice(index, 1);
+  }
+
+  private onAddMissingVariable(variable: string): void {
+    this.edit.variables.push(variable);
+  }
+
+  private onSave(): OnSaveResult {
+    const command = this.entry.parameters.join("\n").trim();
+    if (command.length === 0) {
+      this.entry.error = "The command cannot be empty";
+      return { outcome: OnSaveOutcome.KeepEditing } as OnSaveResult;
+    }
+
+    if (this.hasChanged()) {
+      return { outcome: OnSaveOutcome.Changed, entry: this.edit } as OnSaveResult;
+    }
+
+    return { outcome: OnSaveOutcome.NotChanged } as OnSaveResult;
+  }
+
+  private hasChanged() {
+    return this.entry.parameters.join("\n") !== this.edit.parameters.join("\n");
+  }
+
+  private static variableNameRegex(): RegExp {
+    return /\${[A-Z0-9_-]+}/;
   }
 }
 </script>
@@ -30,5 +233,31 @@ pre {
   padding: 5px;
   background-color: black;
   color: greenyellow;
+}
+
+input[type="text"] {
+  font-size: 1em;
+  font-weight: bold;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-top: 2px;
+  margin-top: 0;
+  width: 99%;
+  color: #2c3e50;
+}
+
+textarea {
+  width: 99%;
+  min-height: 200px;
+  font-family: monospace;
+  font-size: 1em;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
 </style>
--- tests/unit/components/renderers/Command.spec.ts
+++ tests/unit/components/renderers/Command.spec.ts
@@ -2,60 +2,62 @@ import Command from "@/components/renderers/Command.vue";
 import { shallowMount } from "@vue/test-utils";
 
 describe("Command", () => {
-  it("displays the command with working directory", async () => {
-    /* Given */
-    const entry = {
-      type: "command",
-      workingDirectory: "hello-world",
-      parameters: ["java -jar hello-world.jar"],
-    };
+  describe("View/Edit", () => {
+    it("displays the command with working directory", async () => {
+      /* Given */
+      const entry = {
+        type: "command",
+        workingDirectory: "hello-world",
+        parameters: ["java -jar hello-world.jar"],
+      };
 
-    /* When */
-    const wrapper = shallowMount(Command, { props: { entry } });
+      /* When */
+      const wrapper = shallowMount(Command, { props: { entry } });
 
-    /* Then */
-    expect(wrapper.find("pre").text()).toEqual("hello-world $ java -jar hello-world.jar");
-  });
+      /* Then */
+      expect(wrapper.find("pre").text()).toEqual("hello-world $ java -jar hello-world.jar");
+    });
 
-  it("displays the command with variable values", async () => {
-    /* Given */
-    const entry = {
-      type: "command",
-      parameters: ["echo ${NAME} ${NAME}"],
-      variables: ["NAME"],
-      values: {
-        NAME: "Albert",
-      },
-    };
+    it("displays the command with variable values", async () => {
+      /* Given */
+      const entry = {
+        type: "command",
+        parameters: ["echo ${NAME} ${NAME}"],
+        variables: ["NAME"],
+        values: {
+          NAME: "Albert",
+        },
+      };
 
-    /* When */
-    const wrapper = shallowMount(Command, { props: { entry } });
+      /* When */
+      const wrapper = shallowMount(Command, { props: { entry } });
 
-    /* Then */
-    expect(wrapper.find("pre").text()).toEqual("$ echo Albert Albert");
-  });
+      /* Then */
+      expect(wrapper.find("pre").text()).toEqual("$ echo Albert Albert");
+    });
 
-  it("renders the single line command", async () => {
-    /* Given */
-    const entry = { parameters: ['echo "Albert Attard"'] };
+    it("renders the single line command", async () => {
+      /* Given */
+      const entry = { parameters: ['echo "Albert Attard"'] };
 
-    /* When */
-    const wrapper = shallowMount(Command, { props: { entry } });
+      /* When */
+      const wrapper = shallowMount(Command, { props: { entry } });
 
-    /* Then */
-    expect(wrapper.find("pre").text()).toEqual('$ echo "Albert Attard"');
-  });
+      /* Then */
+      expect(wrapper.find("pre").text()).toEqual('$ echo "Albert Attard"');
+    });
 
-  it("renders the multi-line command", async () => {
-    /* Given */
-    const entry = { parameters: ["curl \\", '  --location "https://somewhere.com/" \\', "  --output file.txt"] };
+    it("renders the multi-line command", async () => {
+      /* Given */
+      const entry = { parameters: ["curl \\", '  --location "https://somewhere.com/" \\', "  --output file.txt"] };
 
-    /* When */
-    const wrapper = shallowMount(Command, { props: { entry } });
+      /* When */
+      const wrapper = shallowMount(Command, { props: { entry } });
 
-    /* Then */
-    expect(wrapper.find("pre").text()).toEqual(
-      '$ curl \\\n  --location "https://somewhere.com/" \\\n  --output file.txt'
-    );
+      /* Then */
+      expect(wrapper.find("pre").text()).toEqual(
+        '$ curl \\\n  --location "https://somewhere.com/" \\\n  --output file.txt'
+      );
+    });
   });
 });
